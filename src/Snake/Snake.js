952f89517247fced89975d7e49c9af7694cd573a
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-30 09:47:53 +0100

    rendering array

--- src/Snake/Snake.js
+++ src/Snake/Snake.js
@@ -3,20 +3,36 @@ import React from 'react'
 class Snake extends React.Component {
     state = {
         gameBoard: Array(this.props.boardDimension)
-            .fill(Array(this.props.boardDimension))
-            .fill(1)
+            .fill(
+                Array(this.props.boardDimension).fill(1)
+            )
     }
 
     render() {
+        const gameBoard = JSON.parse(JSON.stringify(this.state.gameBoard)) //kopia tablicy bez zmieniania stanu
+
         return (
             <div>
-                Snake
+                {
+                    gameBoard.map(row => (
+                        <div>
+                            {
+                                row.map(cell => (
+                                    <div>
+                                        {cell}
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    ))
+                }
             </div>
         )
     }
 }
 
 Snake.defaultProps = {
+    // @TODO it should check if number is bigger than 5
     boardDimension: 10
 }
 
